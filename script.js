64259a7cfdc6a66d826f654debe87ea1e82c2d84
Kerolos Emad <58334774+kerolos350@users.noreply.github.com>
2024-12-08 19:47:43 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,65 @@
+var disp = document.getElementById("sum");
+var openBrackets = 0;
+
+function ChV(num) {
+  const lastChar = disp.innerText.slice(-1);
+
+  if (lastChar === ")") {
+    disp.innerText += "*";
+  }
+
+  if (disp.innerText === "0") {
+    disp.innerText = num;
+  } else {
+    disp.innerText += num;
+  }
+}
+
+function Op(op) {
+  const lastChar = disp.innerText.slice(-1);
+
+  if (op === "sb") {
+    if (openBrackets > 0 && (!isNaN(lastChar) || lastChar === ")")) {
+      disp.innerText += ")";
+      openBrackets--;
+    } else {
+      if (!isNaN(lastChar) || lastChar === ")") {
+        disp.innerText += "*";
+      }
+      disp.innerText += "(";
+      openBrackets++;
+    }
+  } else if (op === "AC") {
+    disp.innerText = "0";
+  } else if (op === "del") {
+    const deletedChar = disp.innerText.slice(-1);
+    if (deletedChar === "(") openBrackets--;
+    if (deletedChar === ")") openBrackets++;
+    disp.innerText = disp.innerText.slice(0, -1) || "0";
+  } else if (op === "×") {
+    disp.innerText += "*"
+    disp.innerText = eval(disp.innerText);
+  } else if (op === "÷") {
+    disp.innerText += "/"
+    disp.innerText = eval(disp.innerText);
+  } else if (op === "=") {
+    try {
+      for (let i = 0; i < openBrackets; i++) {
+        disp.innerText += ")";
+      }
+      openBrackets = 0;
+      
+      if (!isNaN(lastChar) || lastChar === ")") {
+        disp.innerText = eval(disp.innerText);
+      } else {
+        disp.innerText = "Error";
+      }
+    } catch (error) {
+      disp.innerText = "Error";
+    }
+  }else {
+    if (!isNaN(lastChar) || lastChar === ")" || (op !== ")" && lastChar !== "(")) {
+      disp.innerText += op;
+    }
+  }
+}
